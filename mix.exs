3a9d9987bd3f94e5cb5305a21e1a4d0613a4ad8b
koga1020 <koga.shozo1020@gmail.com>
2019-11-10 01:27:55 +0900

    update mix

--- mix.exs
+++ mix.exs
@@ -29,8 +29,9 @@ defmodule ExMachinaGen.MixProject do
   defp deps do
     [
       {:credo, "~> 1.1.5", only: [:dev, :test], runtime: false},
-      {:inflex, "~> 2.0.0"},
-      {:ex_doc, ">= 0.0.0", only: :dev}
+      {:ecto, "~> 3.2.5", only: [:dev, :test], runtime: false},
+      {:inflex, "~> 2.0.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
 
