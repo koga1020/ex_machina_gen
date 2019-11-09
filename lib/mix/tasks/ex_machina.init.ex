7d8e6f16d6b0f517a80c807d1742d60f0a39d155
koga1020 <koga.shozo1020@gmail.com>
2019-11-09 23:55:54 +0900

    format

--- lib/mix/tasks/ex_machina.init.ex
+++ lib/mix/tasks/ex_machina.init.ex
@@ -1,5 +1,4 @@
 defmodule Mix.Tasks.ExMachina.Init do
-
   use Mix.Task
 
   def run(args) do
@@ -8,10 +7,13 @@ defmodule Mix.Tasks.ExMachina.Init do
     ecto_option = Keyword.get(option, :ecto, true)
 
     binding = [
-      module:  Mix.Project.config()[:app] |> to_string() |> Macro.camelize,
+      module: Mix.Project.config()[:app] |> to_string() |> Macro.camelize(),
       ecto: ecto_option
     ]
-    Mix.Generator.create_file("test/support/factory/factory.ex", EEx.eval_file("priv/templates/ex_machina.init/factory.ex", binding))
-  end
 
+    Mix.Generator.create_file(
+      "test/support/factory/factory.ex",
+      EEx.eval_file("priv/templates/ex_machina.init/factory.ex", binding)
+    )
+  end
 end
