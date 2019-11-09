678c11d11ebcf55b53bf888ad36ef227ef808626
koga1020 <koga.shozo1020@gmail.com>
2019-11-09 23:57:14 +0900

    rm no need func

--- lib/ex_machina_gen.ex
+++ lib/ex_machina_gen.ex
@@ -1,18 +1,2 @@
 defmodule ExMachinaGen do
-  @moduledoc """
-  Documentation for ExMachinaGen.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExMachinaGen.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
