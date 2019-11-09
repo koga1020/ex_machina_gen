c83d2fea482b37c8d7f0f9c4ab5476f2ebe97ef6
koga1020 <koga.shozo1020@gmail.com>
2019-11-10 00:33:29 +0900

    update elixir version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExMachinaGen.MixProject do
     [
       app: :ex_machina_gen,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "ExMachinaGen",
